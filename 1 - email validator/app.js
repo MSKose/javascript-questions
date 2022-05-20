0f8d6897de4f74785cdae8f969dc08060e8a167f
MSKose <saitmustafa1998@gmail.com>
2022-05-20 23:41:02 +0200

    first commit

---
+++ 1 - email validator/app.js
@@ -0,0 +1,14 @@
+// let x = prompt("enter a emial");
+let x = "ejfbebe";
+let regex = /ˆ\S/;
+// if (x == regex) {
+//         console.log(rege`${x} is a valid e-mail address`);
+// } else {
+//         console.log(`${x} is not a valid address`)
+// };
+
+
+
+
+
+// document.getElementById("demo").innerHTML = regex.test(x);
