6f333311eb0fb649e35daa5c63b087d4ea1d4575
MSKose <saitmustafa1998@gmail.com>
2022-05-25 16:33:20 +0200

    answer done

---
+++ 2 - equalTo100/app.js
@@ -0,0 +1,15 @@
+// Write a JS program to check two numbers and 
+// return true if one of the number is 100 or if 
+// the sum of the two numbers is 100
+
+
+let equalTo100 = (a,b) => a === 100 || b === 100 || a+b === 100;
+
+
+// Lets try some logs
+
+console.log(equalTo100(50,50));
+console.log(equalTo100(0,99));
+console.log(equalTo100(100,0));
+console.log(equalTo100(0,100));
+
