027046d8bab0369b6364a15404b1b0d4e21ab95b
MSKose <saitmustafa1998@gmail.com>
2022-05-25 16:34:54 +0200

    m

--- 2 - equalTo100/app.js
+++ 2 - equalTo100/app.js
@@ -8,8 +8,8 @@ let equalTo100 = (a,b) => a === 100 || b === 100 || a+b === 100;
 
 // Lets try some logs
 
-console.log(equalTo100(50,50));
-console.log(equalTo100(0,99));
-console.log(equalTo100(100,0));
-console.log(equalTo100(0,100));
+console.log(equalTo100(50,50)); //true
+console.log(equalTo100(0,99)); //false
+console.log(equalTo100(100,0)); //true
+console.log(equalTo100(0,100)); //true
 
