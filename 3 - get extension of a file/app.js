42e3dcffc6d09e64ca75ee87619bad657dd4c5b5
MSKose <63124871+MSKose@users.noreply.github.com>
2022-05-25 16:59:57 +0200

    Update app.js

--- 3 - get extension of a file/app.js
+++ 3 - get extension of a file/app.js
@@ -13,3 +13,6 @@ console.log(sliced);
 const fileExtension = (fileName) => fileName.slice(fileName.lastIndexOf('.') + 1);
 
 console.log(fileExtension("app.js"));
+
+//second solution is better because if we had a dot in the file name, indexOf() 
+// would have divided the string from the wrong dot onwards
