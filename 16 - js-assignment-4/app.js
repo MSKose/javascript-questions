9d1f99a9c4e3a07d327de9d0ddf5e428635d1e01
MSKose <saitmustafa1998@gmail.com>
2022-05-30 19:27:56 +0200

    second try, not succesful as of now

--- 16 - js-assignment-4/app.js
+++ 16 - js-assignment-4/app.js
@@ -1,65 +1,68 @@
 //! maaslar dizisi icin maasi ortalamanin altinda olanlara 
 //! %20 zam yaparak zamli maaslari ayrı bir diziye atalım.
 
-const maaslar = [3000, 5000, 4000, 6000, 6500];
+// const maaslar = [3000, 5000, 4000, 6000, 6500];
 
-const ort = maaslar.reduce((x,y) => x+y, 0) / maaslar.length
+// const ort = maaslar.reduce((x,y) => x+y, 0) / maaslar.length
 
-const maaslar2 = maaslar.filter(x => x < ort).map(x => Math.round(x*1.20))
+// const maaslar2 = maaslar.filter(x => x < ort).map(x => Math.round(x*1.20))
 
-console.log(maaslar2);
+// console.log(maaslar2);
 
 
 
 //! Kisiler dizisi icin asagıdaki islemleri gerceklestiren kodları yazınız
-//! 1: Yasi(age) 33 den kücük olan kisilerin adlarini (name) listeyiniz.
-//! 2: 33 yasindan kücüklerin isimlerini diziye saklayiniz.
-//! 3: Meslegi developer olanlarin isim ve yaslarini yeni bir Obje olarak yeni diziye saklayiniz.
+
+
 
 const kisiler = [ 
     { name: 'Ahmet', 
     surname: 'Can', 
     job: 'developer', 
-    age: 30, }, 
+    age: 30}, 
     
     { name: 'Mehmet', 
     surname: 'Baki', 
     job: 'tester', 
-    age: 35, }, 
-
-    { name: 'Nur', 
-    surname: 'Ersan', 
+    age: 35}, 
+    
+    { name: 'Nur',  surname: 'Ersan', 
     job: 'team lead', 
-    age: 40, }, 
-
+    age: 40}, 
+    
     { name: 'Merve', 
     surname: 'Veli', 
     job: 'developer', 
-    age: 26, },
-
+    age: 26},
+    
     { name: 'Ruzgar', 
     surname: 'Kuzey', 
     job: 'tester', 
-    age: 24, }, 
+    age: 24} 
 ];
 
 
-//! sol-1
+//! 1: Yasi(age) 33 den kücük olan kisilerin adlarini (name) listeyiniz.
 
-for (let i of kisiler) {
-    if (i.age < 33){
-        console.log(i.name);
+// for (let i of kisiler) {
+//     if (i.age < 33){
+//         console.log(i.name);
+//     }
+// }
+
+//! 2: 33 yasindan kücüklerin isimlerini diziye saklayiniz.
+
+for (let i in kisiler) {
+    if (kisiler[i].age < 33){
+        // let arr = []
+        // arr[i] = kisiler[i].name
+        // console.log(arr);
+        let newarr = Object.values(kisiler[i])
+        let arr[i] = newarr[0]
+        console.log(arr); 
     }
 }
 
-//! sol-2
 
-for (let i of kisiler) {
-    if (i.age < 33){
-        let nameArr = []
-        nameArr += i.name
-        nameArr
-        Object.values(i)[0]
-        console.log(Object.values(i));
-    }
-}
+//! 3: Meslegi developer olanlarin isim ve yaslarini yeni bir Obje olarak yeni diziye saklayiniz.
+
