6ad6f54d9893654900090f5c3fe5594fb927929b
MSKose <saitmustafa1998@gmail.com>
2022-07-03 06:27:04 +0200

    added the 84th

---
+++ 84 - parts of a list/app.js
@@ -0,0 +1,37 @@
+//! https://www.codewars.com/kata/56f3a1e899b386da78000732
+//! Write a function partlist that gives all the ways to divide a list 
+//! (an array) of at least two elements into two non-empty parts.
+
+//! Each part will be in a string
+//! Elements of a pair must be in the same order as in the original array.
+//! Examples of returns in different languages:
+
+//! a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+//! [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+//! or
+//!  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+//! {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+//! or
+//! a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+//! [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+//! or 
+//! a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+//! [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+//! or
+//! a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+//! "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+
+
+
+function partlist(arr) {
+    let newArr = []
+    for (let i = 1; i < arr.length; i++) {
+      newArr.push([arr.slice(0,i).join(' '), arr.slice(i).join(' ')])
+    } 
+    return newArr
+} 
+console.log(partlist(["I", "wish", "I", "hadn't", "come"]));
+
+
+//! alternative sol-1
+var partlist=a=>a.map((v,i)=>[a.slice(0,i).join(' '),a.slice(i).join(' ')]).slice(1)
