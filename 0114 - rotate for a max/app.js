e054e87d2467285f835390c82a09ebc6e6b2e1cb
MSKose <saitmustafa1998@gmail.com>
2022-07-24 10:27:46 +0200

    added the 114th

---
+++ 0114 - rotate for a max/app.js
@@ -0,0 +1,31 @@
+/*
+https://www.codewars.com/kata/56a4872cbb65f3a610000026/javascript
+
+Let us begin with an example:
+
+Take a number: 56789. Rotate left, you get 67895.
+
+Keep the first digit in place and rotate left the other digits: 68957.
+
+Keep the first two digits in place and rotate the other ones: 68579.
+
+Keep the first three digits and rotate left the rest: 68597. Now it is 
+over since keeping the first four it remains only one digit which 
+rotated is itself.
+
+You have the following sequence of numbers:
+
+56789 -> 67895 -> 68957 -> 68579 -> 68597
+
+and you must return the greatest: 68957.
+*/
+
+function maxRot(n) {
+    let str = String(n)
+    let arr = [~~str]
+    for (let i=0; i<str.length-1; i++){
+        str = str.substring(0, i) + str.substring(i+1) + str[i]
+        arr.push(~~str)
+    }
+    return Math.max(...arr)
+}
